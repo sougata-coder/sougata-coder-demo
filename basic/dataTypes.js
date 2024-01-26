2b05d590f1f73eaa4cd27eb9b37899a80c63e624
sougata-coder <157133207+sougata-coder@users.noreply.github.com>
2024-01-26 15:36:45 +0000

    dataType

---
+++ basic/dataTypes.js
@@ -0,0 +1,18 @@
+// "use strict"; // treat all js node as newer version
+// alert(3+3)  // we are using alert nodejs,not browser
+ let age=20
+ let isLoggedIn=false
+ let state= null
+ let name="sougata"
+
+ // number => 2 to power 53 range
+ // bright
+ // string => ""
+ // boolean => true/false
+ // null=> standalone value/ undefined value 
+ // null=> type is object
+ //symbol=> unique
+ //object
+
+ console.log(typeof null); // object type
+ console.log(typeof undefined); // undefined type
---
+++ basic/variables.js
@@ -0,0 +1,16 @@
+const accountId=1234567
+let accountEmail="sougata@gmail.com"
+var accountPassword="98765"
+accountCity="panskura"
+let accountState;
+
+accountEmail="sangi@gmail.com"
+accountPassword="56789"
+accountCity="kolkata"
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState]);
